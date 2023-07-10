edabbebd381a78a5473b2aa0168f3eed705cb6a1
Kris Kurzawa <kurzawa1@yahoo.com>
2023-07-09 23:48:55 -0400

    no progress

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,7 +4,7 @@ var wind = document.querySelector('#currentWind');
 var humidity = document.querySelector('#currentHumidity');
 var searchBtn = document.querySelector("#btn");
 var todayDisplay = document.querySelector("#today");
-var cities = [];
+
 
 
 // var responseText = document.getElementById('response-text');
@@ -32,79 +32,14 @@ searchBtn.addEventListener("click", function(){
   $('#currentDay').text(today.format('MMM D, YYYY'));
   var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
   getApi(queryURL);
-
-  renderCities();
-})
+  })
 
 
 
 
-// fetch(queryURL.data)
-
-// var searchInput = document.querySelector("#input");
-// var searchForm = document.querySelector("#search-form");
-var searchList = document.querySelector("#searchList");
-// var targetCity = document.querySelector("#result-text");
-
-var cities = [];
 
 
 
-// // Creates a list item for each City Submitted
-function renderCities() {
-  searchList.innerHTML = "";
-
-  for (var i = 0; i < cities.length; i++) {
-    var chosenCity = cities[i];
-
-    var h3 = document.createElement("h3");
-    h3.textContent = City;
-    h3.setAttribute("data-index", i);
-    searchList.appendChild(h3);
-  }
-}
-
-
-// // Stores Cities to Local Storage
-// function storeCities() {
-//   localStorage.setItem("Cities", JSON.stringify(cities));
-// }
-
-// function displayCity() {
-//   var test = document.querySelector('#input').value;
-  
-//   // targetCity.innerHTML = "";
-//   console.log(test);
-//   // // for (var i = 0; i < cities.length; i++) {
-//   //   var chosenCityDisplay = targetCity;
-
-//   //   var h3main = document.createElement("h3");
-//   //   h3main.textContent = chosenCityDisplay;
-//   //   h3main.setAttribute(chosenCityDisplay);
-//   //   targetCity.appendChild(h3main);
-//   // // }
-  
-// }
-
-// // Submits text input
-// searchBtn.addEventListener("click", function(event) {
-//   event.preventDefault();
-//   var searchText = searchInput.value.trim();
-
-//   if (searchText === "") {
-//     return;
-//   }
-
-//   cities.push(searchText);
-//   searchInput.value = "";
-
-//   // console.log(searchText);
-//   searchText = document.querySelector('#result-text');
-
-//   storeCities();
-//   renderCities();
-//   displayCity();
-// });
 
 
 
