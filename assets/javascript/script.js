f52979dc806f703f2020f30b923db5b2f83eedc7
Kris Kurzawa <kurzawa1@yahoo.com>
2023-07-12 13:06:54 -0400

    cleaned up code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -40,7 +40,6 @@ function clearBox() {
 }
 
 function makeForecast(data){
-  // console.log(data)
   var cardsContainer = document.getElementById("cards");
   clearBox();
   
@@ -51,12 +50,10 @@ function makeForecast(data){
 }
 
 function makeForecastCard(data){
-  // console.log(data)
   var card = document.createElement("div");
   card.className = "card col-12 col-md-2 flex-column justify-content-center align-center ms-4 me-3 border border-dark text-center"
   var cardHeader = document.createElement("h5");
   var date = new Date(data.dt*1000)
-  // console.log(date.getDate());
   cardHeader.innerHTML = dayjs.unix(data.dt).utc().format('MMM D');
   card.appendChild(cardHeader);
   var cardTemp = document.createElement("h6");
@@ -69,10 +66,8 @@ function makeForecastCard(data){
   cardHUM.innerHTML = "HUM: " + data.main.temp + " %";
   card.appendChild(cardHUM);
   return card;
+}
   
-  }
-  
-
 function saveToStorage(){
   var newCity = document.querySelector("#input");
   var cityText = newCity.value;
@@ -82,22 +77,10 @@ function saveToStorage(){
   }
 
   cities.push(cityText);
-  // console.log(newCity.value);
   localStorage.setItem("Cities", JSON.stringify(cities));
   return;
-
-  // my storage is what dataType? Array
-  // add an element to an array
-  //if my array is too long now, maybe get rid of the oldest one
-  // SAVE IT TO STORAGE AND STRIGNIFY THE ARRAY
 }
 
-// function textBox(){
-//   var display = document.querySelector("#test");
-//   display.innerHTML
-//   display.append(city);
-
-// }
 function loadStorage(){
   var city = document.querySelector("#input").value;
   var el = document.createElement('h4');
@@ -105,7 +88,6 @@ function loadStorage(){
   el.innerHTML = city;
   document.getElementById('searchList').appendChild(el);
   el.addEventListener('click', function(){
-  console.log(city);
   document.getElementById("targetCity").innerHTML=city;
   var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
   getApi(weatherURL, "weather");
@@ -113,43 +95,13 @@ function loadStorage(){
   getApi(forecastURL, "forecast")
   clearText();
   })
-  // todoList.innerHTML = "";
-  // todoCountSpan.textContent = todos.length;
-
-  // // Render a new li for each todo
-  // for (var i = 0; i < todos.length; i++) {
-  //   var todo = todos[i];
-
-  //   var li = document.createElement("li");
-  //   li.textContent = todo;
-  //   li.setAttribute("data-index", i);
-
-  //   var button = document.createElement("button");
-  //   button.textContent = "Complete ✔️";
-
-  //   li.appendChild(button);
-  //   todoList.appendChild(li);
-    // Clear todoList element and update todoCountSpan
-   
-  
-    // Render a new li for each todo
-
-  //read our storage, parse it
-  //if there is no stroage, so we return
-  // Here we would empty the parent container
-  // then loop through the array and render the batch
-  // add event listener, and then call (getAPI with the city in the button)
-  //make sure the city here comes from the button's innerHTML
-  // var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
-  // getApi(weatherURL, "weather");
-  // var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
-  // getApi(forecastURL, "forecast")  
-// }
 }
+
 function clearText(){
   var clearCity = document.querySelector("#input");
   clearCity.value = "";
 }
+
 searchBtn.addEventListener("click", function(){
   var city = document.querySelector("#input").value;
   document.getElementById("targetCity").innerHTML=city;
@@ -167,7 +119,6 @@ searchBtn.addEventListener("click", function(){
   cardsHeader.style.display = "block";
   var searchHeader = document.querySelector("#searchHeader");
   searchHeader.style.display = "block";
-  
 })
 
 
