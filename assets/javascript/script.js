d7e711391b1aa117f83f5a350f027de949ea63e0
Kris Kurzawa <kurzawa1@yahoo.com>
2023-08-02 23:05:48 -0400

    polish

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,54 +6,54 @@ var searchBtn = document.querySelector("#btn");
 var todayDisplay = document.querySelector("#today");
 var cities = []
 
-function makeWeather(data){
+function makeWeather(data) {
   saveToStorage();
   temp = data.main.temp;
   wind = data.wind.speed;
   humidity = data.main.humidity;
-  document.getElementById("currentTemp").innerHTML=temp;
-  document.getElementById("currentWind").innerHTML=wind;
-  document.getElementById("currentHumidity").innerHTML=humidity;
+  document.getElementById("currentTemp").innerHTML = temp;
+  document.getElementById("currentWind").innerHTML = wind;
+  document.getElementById("currentHumidity").innerHTML = humidity;
 }
 
 function getApi(queryURL, type) {
   fetch(queryURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    if(type === "weather"){
-      makeWeather(data)
-    } else{
-      makeForecast(data)
-    }
-   
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (type === "weather") {
+        makeWeather(data)
+      } else {
+        makeForecast(data)
+      }
+
     });
 }
 
 function clearBox() {
   var div = document.getElementById("cards");
-    
-  while(div.firstChild) {
-      div.removeChild(div.firstChild);
+
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
   }
 }
 
-function makeForecast(data){
+function makeForecast(data) {
   var cardsContainer = document.getElementById("cards");
   clearBox();
-  
+
   for (let index = 0; index < data.list.length; index += 8) {
     var card = makeForecastCard(data.list[index])
     cardsContainer.appendChild(card);
   }
 }
 
-function makeForecastCard(data){
+function makeForecastCard(data) {
   var card = document.createElement("div");
-  card.className = "card col-12 col-md-2 flex-column justify-content-center align-center ms-4 me-3 border border-dark text-center"
+  card.className = "card col-12 col-md-2 flex-column justify-content-center align-center ms-4 me-1 border border-dark text-center"
   var cardHeader = document.createElement("h5");
-  var date = new Date(data.dt*1000)
+  var date = new Date(data.dt * 1000)
   cardHeader.innerHTML = dayjs.unix(data.dt).utc().format('MMM D');
   card.appendChild(cardHeader);
   var cardTemp = document.createElement("h6");
@@ -67,8 +67,8 @@ function makeForecastCard(data){
   card.appendChild(cardHUM);
   return card;
 }
-  
-function saveToStorage(){
+
+function saveToStorage() {
   var newCity = document.querySelector("#input");
   var cityText = newCity.value;
 
@@ -81,36 +81,36 @@ function saveToStorage(){
   return;
 }
 
-function loadStorage(){
+function loadStorage() {
   var city = document.querySelector("#input").value;
   var el = document.createElement('h4');
   el.className = "element";
   el.innerHTML = city;
   document.getElementById('searchList').appendChild(el);
-  el.addEventListener('click', function(){
-  document.getElementById("targetCity").innerHTML=city;
-  var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
-  getApi(weatherURL, "weather");
-  var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
-  getApi(forecastURL, "forecast")
-  clearText();
+  el.addEventListener('click', function () {
+    document.getElementById("targetCity").innerHTML = city;
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+    getApi(weatherURL, "weather");
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
+    getApi(forecastURL, "forecast")
+    clearText();
   })
 }
 
-function clearText(){
+function clearText() {
   var clearCity = document.querySelector("#input");
   clearCity.value = "";
 }
 
-searchBtn.addEventListener("click", function(){
+searchBtn.addEventListener("click", function () {
   var city = document.querySelector("#input").value;
-  document.getElementById("targetCity").innerHTML=city;
+  document.getElementById("targetCity").innerHTML = city;
   var today = dayjs();
   $('#currentDay').text(today.format('MMM D, YYYY'));
   var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
   getApi(weatherURL, "weather");
   var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
-  getApi(forecastURL, "forecast")  
+  getApi(forecastURL, "forecast")
   loadStorage();
   saveToStorage();
   clearText();
